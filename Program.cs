2ece2c9abb83ffaa97a46d74aa9082350b970232
Dario Marzzucco <123565631+DMarzzucco@users.noreply.github.com>
2024-09-02 16:35:40 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
-namespace Api
+namespace MiApiRest
 {
     public class Program
     {
@@ -10,11 +10,11 @@ namespace Api
             CreateHostBuilder(args).Build().Run();
         }
 
-        public statix IHostBuilder CreateHostBuilder(string[] args) =>
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-                webBuilder.UseStartup<Startup>();
-            })
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
     }
 }
